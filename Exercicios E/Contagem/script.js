5a6574e08f4e9fc65576a1d494ef2fb9c672d908
AllanCFE <allan.echeverria@gmail.com>
2022-04-05 00:32:32 -0300

    Contador: add emojis

--- Exercicios E/Contagem/script.js
+++ Exercicios E/Contagem/script.js
@@ -8,17 +8,18 @@ function contar (){
     if(inicio.value == "" || fim.value == "" || passo.value == "") alert('Preencha todos os campos')
     else if(passo.value == 0 || passo < 0 || inicio.value == fim.value || Math.abs(fim.value - inicio.value) < Math.abs(passo.value)) alert("Configuração inválida")
     else{
+        res.innerHTML = ""
         console.log('s')
         res.innerHTML += inicio.value
         if(inicio.value < fim.value){
             for(let i = Number(inicio.value)+Number(passo) ; i <= Number(fim.value) ; i+= passo){
-                res.innerHTML += ", " + i
+                res.innerHTML += " \u{1F449} " + i
             }
         } else {
             for(let i = Number(inicio.value)+Number(passo) ; i >= Number(fim.value) ; i-= passo){
-                res.innerHTML += ", " + i
+                res.innerHTML += " \u{1F449} " + i
             }
         }
-        res.innerHTML += " | FIM!"
+        res.innerHTML += " \u{1F449} \u{1F3C1}"
     }
 }
