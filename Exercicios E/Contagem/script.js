a102f32393d0b1623114cc1597210a227ac95ba4
AllanCFE <allan.echeverria@gmail.com>
2022-04-05 00:26:28 -0300

    Contador: Passo absoluto

--- Exercicios E/Contagem/script.js
+++ Exercicios E/Contagem/script.js
@@ -6,7 +6,7 @@ function contar (){
     var res = document.querySelector('#res')
 
     if(inicio.value == "" || fim.value == "" || passo.value == "") alert('Preencha todos os campos')
-    else if(passo.value == 0 || passo.value < 0 && inicio.value < fim.value || passo.value > 0 && inicio.value > fim.value || inicio.value == fim.value || Math.abs(fim.value - inicio.value) < Math.abs(passo.value)) alert("Configuração inválida")
+    else if(passo.value == 0 || passo < 0 || inicio.value == fim.value || Math.abs(fim.value - inicio.value) < Math.abs(passo.value)) alert("Configuração inválida")
     else{
         var res_string = inicio.value
         if(inicio.value < fim.value){
@@ -14,7 +14,7 @@ function contar (){
                 res_string += ", " + i
             }
         } else {
-            for(let i = Number(inicio.value)+Number(passo) ; i >= Number(fim.value) ; i+= passo){
+            for(let i = Number(inicio.value)+Number(passo) ; i >= Number(fim.value) ; i-= passo){
                 res_string += ", " + i
             }
         }
